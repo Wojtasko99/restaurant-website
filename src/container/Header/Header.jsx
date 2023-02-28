091c76984e2535e54e04d889dbb16bc58c906157
Damian Wojtal <106908962+Wojtasko99@users.noreply.github.com>
2023-02-28 19:51:23 +0100

    Add navbar

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import images from '../../constants/images';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header'>
+    <div className='app_header-container'>
+      <div className='app_header-container-side-text'>
+        <p>#Bar</p>
+        <p>#Gericht</p>
+      </div>
+      <div className='app_header-container-header'>
+        <h1>The key to Fine dining</h1>
+        <p>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+        <button type='button'>Explore Menu</button>
+      </div>
+      <div className='app_header-container-image'>
+        <img alt="img-welcome" src={images.welcome}/>
+      </div>
+    </div>
   </div>
 );
 
