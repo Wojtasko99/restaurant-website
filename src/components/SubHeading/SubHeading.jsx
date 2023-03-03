e67f6f16efeb81bc0c0771461dfe88e3ca8f68ce
Damian Wojtal <106908962+Wojtasko99@users.noreply.github.com>
2023-03-03 20:55:08 +0100

    Added SubHeading

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import images from '../../constants/images';
+import './SubHeading.css';
 const SubHeading = () => (
-  <div>
-    SubHeading
+  <div className='app__subheading'>
+    <div className='app__subheading-container'>
+        <h3>Chase the new Flavour</h3>
+        <img aria-disabled src={images.spoon}/>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import images from '../../constants/images';
 import './Header.css';
+import {SubHeading} from '../../components/index';
 
 const Header = () => (
   <div className='app__header'>
@@ -10,6 +11,7 @@ const Header = () => (
         <p>#Gericht</p>
       </div>
       <div className='app_header-container-header'>
+        <SubHeading />
         <h1>The key to Fine dining</h1>
         <p>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
         <button type='button'>Explore Menu</button>
